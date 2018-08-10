10274500414678cdd764d46eac279e6328d2dbac
Jorge Vergara <javebratt@gmail.com>
2018-08-10 15:06:45 -0500

    Checks if there's a logged in user inside the AuthGuard

--- src/app/services/authentication/auth.guard.ts
+++ src/app/services/authentication/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AngularFireAuth } from 'angularfire2/auth';
 
@@ -7,11 +7,21 @@ import { AngularFireAuth } from 'angularfire2/auth';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private afAuth: AngularFireAuth) {}
+  constructor(private afAuth: AngularFireAuth, private router: Router) {}
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
-    return;
+    return new Promise(resolve => {
+      this.afAuth.user.subscribe(user => {
+        if (user) {
+          resolve(true);
+        } else {
+          console.log('User is not logged in');
+          this.router.navigate(['/login']);
+          resolve(false);
+        }
+      });
+    });
   }
 }
